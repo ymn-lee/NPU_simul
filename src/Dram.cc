526e4747bba2a3bfb1522ebebae620f0d09dd7c7
yumin <yung.ym.lee@gmail.com>
2025-09-22 12:56:37 +0900

    [Update] dram_origin addr

--- src/Dram.cc
+++ src/Dram.cc
@@ -206,6 +206,7 @@ void DramRamulator2::push(uint32_t cid, MemoryAccess* request) {
   mf->size = request->size;
   mf->write = request->write;
   mf->request = true;
+  mf->origin_addr = request->dram_address;
   mf->origin_data = request;
   mf->operand_id = request->operand_id;
   _mem[cid]->push(mf);
