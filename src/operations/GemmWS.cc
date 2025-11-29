962122e7f06e206151315c1d42b55b1032d8a8c8
yumin <yung.ym.lee@gmail.com>
2025-11-29 11:15:12 +0900

    [Update] core copy decoding

--- src/operations/GemmWS.cc
+++ src/operations/GemmWS.cc
@@ -388,6 +388,7 @@ void GemmWS::initialize_instructions(Tile* tile, Mapping mapping) {
   if(tick==threshold){
     diviision = total_tiles%_config.num_cores;
   }
+  int cnt = 0;
 
   addr_type act_sp_base_addr = SPAD_BASE;
   addr_type weight_sp_base_addr = SPAD_BASE + mapping.tile_in_loop.N *
@@ -486,6 +487,7 @@ void GemmWS::initialize_instructions(Tile* tile, Mapping mapping) {
       std::set<addr_type> input_set;
       std::set<addr_type> input_set_copy;
       for (int iter_n = 0; iter_n < n_loop; iter_n++) {
+        cnt = 0;
         for (int iter_c = 0; iter_c < c_in_loop; iter_c+=elems_per_access) {
           uint32_t N = N_offset + iter_n;
           uint32_t C = C_offset + iter_c;
@@ -496,8 +498,15 @@ void GemmWS::initialize_instructions(Tile* tile, Mapping mapping) {
           else
             index = {N, C};
           input_set.insert(first_addr + make_address(index, _input_shape));
-          if((iter_n+tile->core_id)%_config.num_cores==0){
-            input_set_copy.insert(first_addr + make_address(index, _input_shape));
+          if(n_loop>8){
+            if((iter_n+tile->core_id)%_config.num_cores==0){
+              input_set_copy.insert(first_addr + make_address(index, _input_shape));
+            }
+          }else{
+            if(cnt%_config.num_cores==tile->core_id){
+              input_set_copy.insert(first_addr + make_address(index, _input_shape));
+            }
+            cnt = (cnt+1)%_config.num_cores;
           }
         }
       }
